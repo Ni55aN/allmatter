e1d9ac89ff5ef4d3fdc5e14462cf0a7a453c6a1c
Vitaliy Stoliarov <huccah@gmail.com>
2018-11-03 21:11:02 +0200

    refactor binary operations

--- src/editor/common/components/binary-operation.js
+++ src/editor/common/components/binary-operation.js
@@ -1,4 +1,4 @@
-import { Component, Input } from 'rete';
+import { Component, Input, Output } from 'rete';
 import Color from 'color';
 import Utils from '../../utils';
 import sockets from '../../sockets';
@@ -8,77 +8,53 @@ export default class BinaryMathComponent extends Component {
     builder(node) {
         const inp1 = new Input('value1', 'Value', sockets.value);
         const inp2 = new Input('value2', 'Value', sockets.value);
-        const out = new Input('value', 'Value', sockets.value);
+        const out = new Output('value', 'Value', sockets.value);
 
         return node
             .addInput(inp1)
             .addInput(inp2)
             .addOutput(out)
     }
-    
-    async calculate (inputs, expression) {
-        let a = inputs['value1'][0];
-        let b = inputs['value2'][0];
 
-        // let sql = Math.sqrt,
-        //     abs = Math.abs;
+    getFunction(expression) {
+        const f = new Function('sqrt, abs, a, b', 'return '+expression);
 
-        if (typeof a === 'number' && typeof b === 'number') 
-            return eval(expression);
+        return f.bind(null, Math.sqrt, Math.abs);
+    }
+
+    order(a, b) {
+        if (a instanceof WebGLTexture)
+            return [a, b]
+
+        if (b instanceof WebGLTexture)
+            return [b, a]
         
-        if (a instanceof WebGLTexture && b instanceof WebGLTexture) {
+        if (a instanceof Color)
+            return [a, b]
+        
+        if (b instanceof Color)
+            return [b, a]
+        
+        return [a, b]
+    }
+
+    async calculate (inputs, expression) {
+        const [a, b] = this.order(inputs['value1'][0], inputs['value2'][0]);
+        const f = this.getFunction(expression);
+
+        if (typeof a === 'number')
+            return f(a, b);
+        
+        if (a instanceof Color)
+            return new Color(f(a.r, b), f(a.g, b), f(a.b, b));
+
+        if (a instanceof WebGLTexture) {
             const result = Utils.createMockCanvas();
-
-            result.blend(a, b, expression);
-
+            const value = b instanceof Color ? b.array() : b;
+    
+            result.blend(a, value, expression);
+    
             return result.toTexture();
-        }
-
-        if (a instanceof Color && b instanceof Color) {
-            // var c1 = a;
-            // var c2 = b;
-
-            const color = new Color();
-
-            [a, b] = [a.r, b.r];
-            eval(expression);
-            [a, b] = [a.g, b.g];
-            eval(expression);
-            [a, b] = [a.b, b.b];
-            eval(expression);
-
-            return color;
-        }
-
-        if (a instanceof WebGLTexture && typeof b === 'number' || ([a, b] = [
-            b, a
-        ], a instanceof WebGLTexture && typeof b === 'number')) {
-
-            const result = Utils.createMockCanvas();
-
-            result.blend(a, b, expression);
-
-            return result.toTexture();
-        }
-
-        if (a instanceof WebGLTexture && b instanceof Color || ([a, b] = [
-            b, a
-        ], a instanceof WebGLTexture && b instanceof Color)) {
-
-            const result = Utils.createMockCanvas();
-
-            result.blend(a, b.toArray(), expression);
-
-            return result.toTexture();
-        }
-
-        if (typeof a === 'number' && b instanceof Color || ([a, b] = [
-            b, a
-        ], typeof a === 'number' && b instanceof Color)) {
-
-            // const color = new Color(f(b.r, a), f(b.g, a), f(b.b, a));
-
-            // return color;
         }
     }
 }
